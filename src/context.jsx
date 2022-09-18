51529d71640866aec9848c0c4df93ee38a981cce
Boyan Velikov <boyanvelikov@gmail.com>
2022-09-18 05:33:07 +0000

    AppProvider

--- src/context.jsx
+++ src/context.jsx
@@ -3,7 +3,9 @@ import React, {useContext} from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
-  return <AppContext.Provider value='hello'>
+  return <AppContext.Provider value={{name: 'john', role: 'student'}}>
     {children}
   </AppContext.Provider>
 }
+
+export {AppContext, AppProvider}
