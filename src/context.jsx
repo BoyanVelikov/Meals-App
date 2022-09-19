0251f56f2d19235045acccc3097458ac8a2d7240
Boyan Velikov <boyanvelikov@gmail.com>
2022-09-19 06:10:18 +0000

    Started to fetch data

--- src/context.jsx
+++ src/context.jsx
@@ -1,11 +1,34 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 
 const AppContext = React.createContext();
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
 const AppProvider = ({children}) => {
+  
+  useEffect(()=>{
+    
+    const fetchData = async() =>{
+      try {
+        const response = await fetch(randomMealUrl)
+        const data = await response.json();
+        console.log(data)
+      } catch (error) {
+                console.log(error)
+      }
+    }
+    fetchData()
+  },[])
+  
   return <AppContext.Provider value={{name: 'john', role: 'student'}}>
     {children}
   </AppContext.Provider>
 }
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+}
+
 
 export {AppContext, AppProvider}
